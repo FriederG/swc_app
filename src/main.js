57d8735c50c1a2d4761c50e0aa40da123dbf7287
FriederG <[frieder.s.griem@student.fh-kiel.de]>
2020-06-04 12:02:25 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -3,11 +3,25 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import VueI18n from 'vue-i18n'
+import messages from './lang'
 
 Vue.config.productionTip = false
+
+//sprachdatei konfigurieren
+//mehr Infos: https://webdeasy.de/vue-i18n-app/
+Vue.use(VueI18n);
+export const i18n = new VueI18n({
+  locale: 'de',
+  fallbackLocale: 'de',
+  messages
+});
 
 new Vue({
   router,
   store,
+  i18n,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
