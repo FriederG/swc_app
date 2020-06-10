0f193ec72f4568ea634e78812d2b68fa6c2d4ce5
Honichstulle <ac3ofsp4des893@gmail.com>
2020-06-10 12:17:50 +0200

    FAQ Test

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -1,1 +1,180 @@
-<template><div>FAQ</div></template>
+<template>
+  <div>
+    <h1>FAQ</h1>
+
+    <h3 class="green--text">Allgemeine Fragen zum Turnier</h3>
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-header>Was ist 1+1?</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <v-expansion-panel-header>Was meine Mutter?</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <v-expansion-panel-header
+          >Was ist der Sinn des Lebens?</v-expansion-panel-header
+        >
+        <v-expansion-panel-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+    <h3 class="green--text">Camping und Parken</h3>
+
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-header>Was ist 1+1?</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <v-expansion-panel-header>Was meine Mutter?</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <v-expansion-panel-header
+          >Was ist der Sinn des Lebens?</v-expansion-panel-header
+        >
+        <v-expansion-panel-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+    <h3 class="green--text">Essen und Verpflegung</h3>
+
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-header>Was ist 1+1?</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <v-expansion-panel-header>Was meine Mutter?</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <v-expansion-panel-header
+          >Was ist der Sinn des Lebens?</v-expansion-panel-header
+        >
+        <v-expansion-panel-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+    <!--
+    <v-container>
+      <v-row>
+
+        <v-card class="mx-auto mt-3S" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://kielrugby.com/wp-content/uploads/2019/10/IMG_2117-1024x683.jpg"
+          >
+            <v-card-title>Turnier</v-card-title>
+          </v-img>
+
+          <v-card-text class="text--primary">
+            Alle Informationen zum Turnier...
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="green" text>
+              Info
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-card class="mx-auto mt-3" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://www.telegraph.co.uk/content/dam/Travel/2018/July/Scotland-campingGettyImages-526564828-xlarge.jpg"
+          >
+            <v-card-title>Camping</v-card-title>
+          </v-img>
+
+          <v-card-text class="text--primary">
+            Alle Informationen zum Camping...
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="green" text>
+              Info
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+
+
+        <v-card class="mx-auto mt-3" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://www.vebwk.com/wp-content/uploads/2018/12/Fotografieren-von-Essen-im-Restaurant.jpg"
+          >
+            <v-card-title>Essen</v-card-title>
+          </v-img>
+
+          <v-card-text class="text--primary">
+            Alle Informationen zum Essen...
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="green" text>
+              Info
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-row>
+    </v-container>
+      -->
+  </div>
+</template>
