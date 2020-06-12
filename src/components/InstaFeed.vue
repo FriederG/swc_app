c9c9091bc5e210ec23bb84fd50856257dea7ce0a
Honichstulle <ac3ofsp4des893@gmail.com>
2020-06-12 15:54:05 +0200

    jquery

--- src/components/InstaFeed.vue
+++ src/components/InstaFeed.vue
@@ -1,27 +1,3 @@
 <template>
-  <vue-instagram
-    token="IGQVJWLXdOVXAtQjkweG5wZAGtLUTRjTkQ5VXlZAWTlYYkdKZA09pVS15OGVfTjF0UjZAVQnFONnBvb2FNbWJKLU1MT1dhNUlRTnRqU2VLUjdoLXBSQkc1d2xJMGFjdDI2cXQ5aWtsQVdSZAUxpSl9Ja2x3TQZDZD"
-    :count="5"
-    :tags="['segeln', 'hashtag2']"
-    mediaType="image"
-  >
-    <template slot="feeds" slot-scope="props">
-      <li class="fancy-list">{{ props.feed.link }}</li>
-    </template>
-    <template slot="error" slot-scope="props">
-      <div class="fancy-alert">{{ props.error.error_message }}</div>
-    </template>
-  </vue-instagram>
+  <div></div>
 </template>
-
-<script>
-import VueInstagram from "vue-instagram";
-
-export default {
-  name: "App",
-
-  components: {
-    VueInstagram,
-  },
-};
-</script>
