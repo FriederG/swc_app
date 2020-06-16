109c6b5c86f6dc21197d68c3f1c2c2a91331067b
Honichstulle <ac3ofsp4des893@gmail.com>
2020-06-16 11:15:34 +0200

    Alkohol-Rechner Counter

---
+++ src/components/Pegelstand.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h1>Pegelstand</h1>
+    <br />
+    <v-simple-table>
+      <table>
+        <tr>
+          <td><b>Bier</b></td>
+          <td>{{ beer_count }}</td>
+          <td>
+            <v-btn v-on:click="more_beer"><v-icon>mdi-beer</v-icon></v-btn>
+          </td>
+        </tr>
+        <tr>
+          <td><b>Wein</b></td>
+          <td>{{ wine_count }}</td>
+          <td>
+            <v-btn v-on:click="more_wine"><v-icon>mdi-beer</v-icon></v-btn>
+          </td>
+        </tr>
+        <tr>
+          <td><b>Shot</b></td>
+          <td>{{ shot_count }}</td>
+          <td>
+            <v-btn v-on:click="more_shots"><v-icon>mdi-beer</v-icon></v-btn>
+          </td>
+        </tr>
+        <tr>
+          <td><b>Drink</b></td>
+          <td>{{ drink_count }}</td>
+          <td>
+            <v-btn v-on:click="more_drinks"><v-icon>mdi-beer</v-icon></v-btn>
+          </td>
+        </tr>
+      </table>
+    </v-simple-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pegelstand",
+
+  data() {
+    return {
+      beer_count: 0,
+      wine_count: 0,
+      shot_count: 0,
+      drink_count: 0,
+    };
+  },
+  methods: {
+    more_beer() {
+      this.beer_count = this.beer_count + 1;
+    },
+    more_wine() {
+      this.wine_count = this.wine_count + 1;
+    },
+    more_shots() {
+      this.shot_count = this.shot_count + 1;
+    },
+    more_drinks() {
+      this.drink_count = this.drink_count + 1;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
